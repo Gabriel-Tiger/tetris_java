2d9fdb99d8221c3ffa9829243c4dcb44d0b710e1
LucasF1211 <lucas.fernandes1211@gmail.com>
2021-12-05 17:16:18 -0300

    Fix sounds

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -8,194 +8,184 @@ import java.util.Random;
 import java.util.concurrent.Delayed;
 
 public class GamePanel extends JPanel implements ActionListener {
-    int DELAY = 200;
+	int DELAY = 200;
 
-    Score score = new Score();
-    int player1Score=0;
-    final int x[] = new int[StaticValues.GAME_UNITS];
-    final int y[] = new int[StaticValues.GAME_UNITS];
-    int spawnX;
-    int spawnY;
-    int flipLock=0;
-    char direction = 'd';
-    boolean running = false;
-    Timer timer;
-    Random random;
+	Score score = new Score();
+	int player1Score = 0;
+	final int x[] = new int[StaticValues.GAME_UNITS];
+	final int y[] = new int[StaticValues.GAME_UNITS];
+	int spawnX;
+	int spawnY;
+	int flipLock = 0;
+	char direction = 'd';
+	boolean running = false;
+	Timer timer;
+	Random random;
 
-    Engine engine;
+	Engine engine;
 
-    GamePanel(){
-        random = new Random();
-        this.setPreferredSize(new Dimension(StaticValues.SCREEN_WIDTH, StaticValues.SCREEN_HEIGHT));
-        this.setBackground(Color.BLACK);
-        this.setFocusable(true);
-        this.addKeyListener(new MyKeyAdapter());
-        startGame();
-    }
-    public void startGame(){
-        newBlock();
-        engine = new Engine(spawnX, spawnY);
-        running = true;
-        timer = new Timer(DELAY,this);
-        timer.start();
+	GamePanel() {
+		random = new Random();
+		this.setPreferredSize(new Dimension(StaticValues.SCREEN_WIDTH, StaticValues.SCREEN_HEIGHT));
+		this.setBackground(Color.BLACK);
+		this.setFocusable(true);
+		this.addKeyListener(new MyKeyAdapter());
+		startGame();
+	}
 
-    }
-    public void paintComponent(Graphics g){
-        super.paintComponent(g);
-        draw(g);
-    }
-    public void draw(Graphics g){
+	public void startGame() {
+		newBlock();
+		engine = new Engine(spawnX, spawnY);
+		running = true;
+		timer = new Timer(DELAY, this);
+		timer.start();
 
-        int table[][] = engine.getTable();
-        //Scan the table matrix and draw
-        for(int j=0;j<(StaticValues.WIDTH_UNIT);j++){
-            for(int k=0;k<(StaticValues.HEIGHT_UNIT);k++){
-                if(table[k][j] != 0){
-                    g.setColor(color(table[k][j]));
-                    g.fillRect((j*StaticValues.UNIT_SIZE), (k*StaticValues.UNIT_SIZE), StaticValues.UNIT_SIZE, StaticValues.UNIT_SIZE);
-                }
-            }
-        }
-        int layer[][] = engine.getLayer();
-        //Scan the prop matrix layer and draw
-        for(int j=0;j<(StaticValues.WIDTH_UNIT);j++){
-            for(int k=0;k<(StaticValues.HEIGHT_UNIT);k++){
-                if(layer[k][j] != 0){
-                    g.setColor(color(layer[k][j]));
-                    g.fillRect((j*StaticValues.UNIT_SIZE), (k*StaticValues.UNIT_SIZE), StaticValues.UNIT_SIZE, StaticValues.UNIT_SIZE);
-                }
-            }
-        }
+	}
 
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		draw(g);
+	}
 
+	public void draw(Graphics g) {
 
+		int table[][] = engine.getTable();
+		// Scan the table matrix and draw
+		for (int j = 0; j < (StaticValues.WIDTH_UNIT); j++) {
+			for (int k = 0; k < (StaticValues.HEIGHT_UNIT); k++) {
+				if (table[k][j] != 0) {
+					g.setColor(color(table[k][j]));
+					g.fillRect((j * StaticValues.UNIT_SIZE), (k * StaticValues.UNIT_SIZE), StaticValues.UNIT_SIZE,
+							StaticValues.UNIT_SIZE);
+				}
+			}
+		}
+		int layer[][] = engine.getLayer();
+		// Scan the prop matrix layer and draw
+		for (int j = 0; j < (StaticValues.WIDTH_UNIT); j++) {
+			for (int k = 0; k < (StaticValues.HEIGHT_UNIT); k++) {
+				if (layer[k][j] != 0) {
+					g.setColor(color(layer[k][j]));
+					g.fillRect((j * StaticValues.UNIT_SIZE), (k * StaticValues.UNIT_SIZE), StaticValues.UNIT_SIZE,
+							StaticValues.UNIT_SIZE);
+				}
+			}
+		}
 
-        g.setColor(Color.darkGray);
-        for(int i = 0; i< StaticValues.SCREEN_HEIGHT/StaticValues.UNIT_SIZE; i++){
-            g.drawLine(i*StaticValues.UNIT_SIZE,0,i* StaticValues.UNIT_SIZE, StaticValues.SCREEN_HEIGHT);
-            g.drawLine(0,i*StaticValues.UNIT_SIZE,StaticValues.SCREEN_WIDTH,i*StaticValues.UNIT_SIZE);
-        }
+		g.setColor(Color.darkGray);
+		for (int i = 0; i < StaticValues.SCREEN_HEIGHT / StaticValues.UNIT_SIZE; i++) {
+			g.drawLine(i * StaticValues.UNIT_SIZE, 0, i * StaticValues.UNIT_SIZE, StaticValues.SCREEN_HEIGHT);
+			g.drawLine(0, i * StaticValues.UNIT_SIZE, StaticValues.SCREEN_WIDTH, i * StaticValues.UNIT_SIZE);
+		}
 
-    }
-    public void newBlock(){
-        spawnX = 0;
-        spawnY = random.nextInt(StaticValues.WIDTH_UNIT);
-    }
-    public void move(){
-        engine.move(direction);
-        direction='d';
-        repaint();
-        flipLock=0;
-    }
-    public void checkCollisions(){
-        //Check down border
-        if (engine.collision() == 2){
-            engine.tableAddPieces();
-            score = engine.lineScoreDetect();
-            if(score.done){
-                player1Score = player1Score +10;
-                System.out.println(player1Score);
-                engine.lineScoreDownAnimation(score);
-                playSound.playSound("point-01.wav"); //nao faz 2 plin plin
-                checkCollisions();
-            }
-            engine.reset();
-        }
-        if (engine.collision() == 3){
-            System.out.println("GAME OVER");
-            playSound.playSound("hit-03.wav");
-            engine.tableAddPieces();
-            engine.reset();
-            running = false;
-        }
+	}
 
+	public void newBlock() {
+		spawnX = 0;
+		spawnY = random.nextInt(StaticValues.WIDTH_UNIT);
+	}
 
-    }
-    public void gameOver(Graphics g){
+	public void move() {
+		engine.move(direction);
+		direction = 'd';
+		repaint();
+		flipLock = 0;
+	}
 
-    }
+	public void checkCollisions() {
+		// Check down border
+		if (engine.collision() == 2) {
+			engine.tableAddPieces();
+			score = engine.lineScoreDetect();
+			if (score.done) {
+				player1Score = player1Score + 10;
+				System.out.println(player1Score);
+				engine.lineScoreDownAnimation(score);
+				playSound.playSound("point-01.wav"); // nao faz 2 plin plin
+				checkCollisions();
+			}
+			engine.reset();
+		}
+		if (engine.collision() == 3) {
+			System.out.println("GAME OVER");
+			playSound.playSound("hit-03.wav");
+			engine.tableAddPieces();
+			engine.reset();
+			running = false;
+		}
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if (running) {
-            checkCollisions();
-            if(engine.collision()!=2){
-            move();
-            }
-        }
-        //repaint();
-    }
+	}
 
-    public class MyKeyAdapter extends KeyAdapter{
-        @Override
-        public void keyPressed(KeyEvent e){
-            switch (e.getKeyCode()){
-                case KeyEvent.VK_LEFT:{
-                    direction='l';
-                    if(engine.collision()==0) {
-                        //move();
-                    }
-                    repaint();
+	public void gameOver(Graphics g) {
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (running) {
+			checkCollisions();
+			if (engine.collision() != 2) {
+				move();
+			}
+		}
+		// repaint();
+	}
+
+	public class MyKeyAdapter extends KeyAdapter {
+		@Override
+		public void keyPressed(KeyEvent e) {
+			switch (e.getKeyCode()) {
+			case KeyEvent.VK_LEFT: {
+				direction = 'l';
+				if (engine.collision() == 0) {
+					// move();
+				}
+				repaint();
 //                    System.out.println(direction);
-                    break;
-                }
-                case KeyEvent.VK_RIGHT:{
-                    direction='r';
-                    if(engine.collision()==0) {
-                        //move();
-                    }
-                    repaint();
+				break;
+			}
+			case KeyEvent.VK_RIGHT: {
+				direction = 'r';
+				if (engine.collision() == 0) {
+					// move();
+				}
+				repaint();
 //                    System.out.println(direction);
-                    break;
-                }
-                case KeyEvent.VK_DOWN:{
-                    direction='d';
-                    if(engine.collision()==0) {
-                        move();
-                    }
-                    repaint();
-                    break;
-                }
-                case KeyEvent.VK_UP:{
-                    if(flipLock == 0){
-                        if(engine.collision()==0) {
-                            engine.flip();
-                            flipLock=1;
-                        }
-                        playSound.playSound("rotate.wav");
-                        repaint();
-                    }
-                    break;
-                }
-            }
-        }
-    }
+				break;
+			}
+			case KeyEvent.VK_DOWN: {
+				direction = 'd';
+				if (engine.collision() == 0) {
+					move();
+				}
+				repaint();
+				break;
+			}
+			case KeyEvent.VK_UP: {
+				if (flipLock == 0) {
+					if (engine.collision() == 0) {
+						engine.flip();
+						flipLock = 1;
+					}
+					playSound.playSound("rotate.wav");
+					repaint();
+				}
+				break;
+			}
+			}
+		}
+	}
 
+	Color color(int a) {
+		if (a == 1)
+			return Color.cyan;
+		else if (a == 2)
+			return Color.BLUE;
+		else if (a == 3)
+			return Color.green;
+		else if (a == 4)
+			return Color.yellow;
 
-    Color color(int a){
-        Color c = Color.red;
-
-        switch (a) {
-            case 1 -> {
-                c = Color.cyan;
-                return c;
-            }
-            case 2 -> {
-                c = Color.BLUE;
-                return c;
-            }
-            case 3 -> {
-                c = Color.green;
-                return c;
-            }
-            case 4 -> {
-                c = Color.yellow;
-                return c;
-            }
-            default -> {
-                c = Color.MAGENTA;
-                return c;
-            }
-        }
-    }
+		return Color.MAGENTA;
+	}
 }
--- src/playSound.java
+++ src/playSound.java
@@ -1,32 +1,24 @@
 import javax.sound.sampled.*;
 import java.io.File;
 import java.io.IOException;
-
-;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class playSound {
-    public static void playSound(final String url) {
+	public static void playSound(String url) {
+		try {
+			Clip clip = AudioSystem.getClip();
 
-        File file = new File(url);
-        AudioInputStream audioStream = null;
-        try {
-            audioStream = AudioSystem.getAudioInputStream(file);
-        } catch (UnsupportedAudioFileException | IOException e) {
-            e.printStackTrace();
-            System.out.println(e);
-        }
-        Clip clip = null;
-        try {
-            clip = AudioSystem.getClip();
-        } catch (LineUnavailableException e) {
-            e.printStackTrace();System.out.println(e);
-        }
-        try {
-            assert clip != null;
-            clip.open(audioStream);
-            clip.setFramePosition(0);
-            clip.start();
-        } catch (LineUnavailableException | IOException e) {
-            e.printStackTrace();System.out.println(e);
-        }
-    }}
+
+			URL audioUrl = SerginhoSimulator.class.getResource("sounds/" + url); 
+			AudioInputStream ais =  AudioSystem.getAudioInputStream(audioUrl); 
+			
+			clip.open(ais);
+			clip.start();
+			
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+	}
+}
