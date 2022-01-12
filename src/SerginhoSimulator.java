40d33199e47600ec428312aee6e0d46934a2fe7e
Gabriel-Tiger <Tiger321001@gmail.com>
2022-01-12 16:54:47 -0300

    Update

--- src/SerginhoSimulator.java
+++ src/SerginhoSimulator.java
@@ -1,3 +1,8 @@
+/*
+Code By: Gabriel Marusak (Git: Gabriel-Tiger)
+With collaboration of: Lucas Matheus (Git: lucasf1211)
+ */
+
 public class SerginhoSimulator {
     //Serginho simulator - pq ele gosta de jogar tetriz
     public static void main(String[] args) {
